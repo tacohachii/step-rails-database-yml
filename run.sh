3edf046361a4b6e0a8cd4c74310f431d01f7a99d
Pieter Joost van de Sande <pj@born2code.net>
2013-06-25 16:30:14 +0200

    Adds validation and logging and bumps version to 0.0.3

--- run.sh
+++ run.sh
@@ -1,3 +1,33 @@
-cp "$WERCKER_STEP_ROOT/templates/$WERCKER_RAILS_DATABASE_YML_SERVICE.yml" "$WERCKER_SOURCE_DIR/config/database.yml"
+template_name=""
 
-info $(cat "$WERCKER_SOURCE_DIR/config/database.yml")
+if [ ! -n "$WERCKER_RAILS_DATABASE_YML_SERVICE" ]; then
+  debug 'service option not specified, looking for services in the environment'
+
+  if [ ! -n "$WERCKER_MYSQL_HOST" ]; then
+    info 'mysql service found'
+    template_name="mysql"
+  elif [ ! -n "$WERCKER_POSTGRESQL_HOST" ]; then
+    info 'postgresql service found'
+    template_name="postgresql"
+  fi
+else
+  debug 'service option specified, will load specified template'
+  template_name="$WERCKER_RAILS_DATABASE_YML_SERVICE"
+fi
+
+info "using template $template_name"
+template_filename="$WERCKER_STEP_ROOT/templates/$template_name.yml"
+
+if [ ! -f "$template_filename" ]; then
+  fail "no template found with name $template_name"
+else
+  config_filename="$WERCKER_SOURCE_DIR/config/database.yml"
+
+  if [ -f "$config_filename" ]; then
+    warn 'config/database.yml already exists and will be overwritten'
+  fi
+
+  cp -f "$template_filename" "$config_filename"
+  info "created database.yml in config directory with content:"
+  info "$(cat "$config_filename")"
+fi
